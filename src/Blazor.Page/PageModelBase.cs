1bdda50434f6c6cdb06add59960c3d1e94eac2d4
Mikael Koskinen <mikael.koskinen@adafy.com>
2019-09-17 20:19:16 +0300

    Fixed packaging issues

---
+++ src/Blazor.Page/PageModelBase.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+
+namespace Blazor.Page
+{
+    public abstract class PageModelBase
+    {
+        public Page Parent { get; set; }
+
+        public virtual void OnInitialized()
+        {
+        }
+
+        public virtual Task OnInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual void OnActivated()
+        {
+        }
+
+        public virtual Task OnActivatedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public string Title
+        {
+            set
+            {
+              SetTitle(value);
+            }
+        }
+
+        public void SetTitle(string newTitle)
+        {
+            if (Parent == null)
+            {
+                return;
+            }
+
+            Parent.SetTitle(newTitle);
+        }
+
+        public async ValueTask<string> GetTitle()
+        {
+            if (Parent == null)
+            {
+                return null;
+            }
+
+            return await Parent.GetTitle();
+        }
+    }
+}
